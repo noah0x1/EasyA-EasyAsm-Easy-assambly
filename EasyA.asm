12c015a38eb63f06cd4a759de53b68243105ed97
Noah1 <noahmarx179@gmail.com>
2026-09-25 19:09:24 +0200

    New EasyA start file ( not ready at all )
    I started and stopped over and over again, i uploaded it now so i will work as much as possible.

---
+++ EasyA.asm
@@ -0,0 +1,45 @@
+
+	section .text
+	global _start
+	global reader
+	global forwarder
+	global flags
+	global scan_1
+	global scan_2
+	global user_Code
+
+user_code:
+				;should be filled with the INstruction you wanna test/scan
+
+
+
+
+
+
+
+
+	
+
+_start:
+	push rbp
+	mov rbp ,rsp
+	sub rsp ,32
+	
+	
+
+flags:
+
+	
+
+forwarder:
+	cmp 
+	
+
+reader:
+	push rbp
+	mov rbp ,rsp
+	sub rsp ,256
+	
+	
+	
+	
